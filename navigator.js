11008f85270af3ea1a9e071946dc2fd1739c2470
JoshmPerry <108098612+JoshmPerry@users.noreply.github.com>
2024-01-24 21:47:43 -0600

    Making Great Progress! 	new file:   AIInsight.html 	new file:   AboutMe.html 	new file:   Experience.html 	new file:   Projects.html 	new file:   classic.css 	modified:   index.html 	new file:   navigator.js

---
+++ navigator.js
@@ -0,0 +1,15 @@
+function toHome(){
+    location.href="/";
+}
+function toAbout(){
+    location.href="/AboutMe";
+}
+function toProjects(){
+    location.href="/Projects";
+}
+function toExperience(){
+    location.href="/Experience";
+}
+function toAI(){
+    location.href="/AIInsight";
+}
